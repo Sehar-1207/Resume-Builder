e0b02c1027f855d95b06b5816799c82e9c2be918
Sehar Ajmal <seharajmal452@gmail.com>
2026-04-11 07:45:13 +0500

    Resume Builder

---
+++ src/components/home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div className="w-full py-2.5 font-medium text-sm text-indigo-800 text-center bg-gradient-to-r from-[#7C43FF] to-[#FDFEFF]">
+            <p><span className="px-3 py-1 rounded-lg text-white bg-indigo-600 mr-2">New</span>AI Features Added</p>
+        </div>
+    );
+};
+
+export default Banner
---
+++ src/pages/Review.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Review() {
+  return (
+    <div>
+      <H1>Review Page</H1>
+    </div>
+  )
+}
+
+export default Review
